cdb263da6fbf5b49e686941e624650a3991fcabb
Tobi Faniran <tobifaniran@gmail.com>
2022-01-16 18:56:57 +0100

    set up login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,28 @@
 import React from "react";
+import illustration from "../images/illustration2.svg";
+import logo from "../images/logo.svg";
+import styled from "styled-components";
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login page</h1>
-    </div>
+    <LoginContainer>
+      <div className="row1">
+        <div className="logo">
+          <img src={logo} alt="fundall logo" />
+        </div>
+        <div className="home-image">
+          <div className="hero-image">
+            <img src={illustration} alt="illustration" />
+          </div>
+
+          <div className="home-content">
+            <span>Welcome back!</span>We miss you.
+          </div>
+        </div>
+      </div>
+    </LoginContainer>
   );
 };
+
+const LoginContainer = styled.div``;
 export default Login;
